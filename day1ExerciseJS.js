cea19630c0e2a2ba77ac365e0eccfb73d440e04b
keerthana0109v <keerthanavijayakumar.vk@gmail.com>
2025-04-10 11:24:58 +0530

    day1 homework -playwright training

---
+++ day1ExerciseJS.js
@@ -0,0 +1,41 @@
+//1. Declare a variable called `name` and 
+// assign it a string value. Declare variables `age` (number) and `isStudent` (boolean) as well. 
+
+let name = "Keerthana"
+let age = 22
+let isStudent = false
+console.log("1. "+name+" is "+age+" years old woman")
+
+//2. Use `const` to declare a variable for a constant value 
+// (e.g., the speed of light). Use `let` for another variable representing a changeable value.
+const PI = 3.14
+let radiusofWheel= 78
+let areaofCarwheel= PI*radiusofWheel*radiusofWheel
+console.log("2. Area of Car Wheel is "+ areaofCarwheel)
+
+//3. Create two string variables `firstName` and `lastName`. Concatenate them and print the full name.
+
+let firstName ='Suresh'
+let lastName = 'Raina'
+console.log("3. Fullname: "+firstName+lastName)
+
+//4. Addition:Declare two variables `a` and `b` with numeric values. Print their sum to the console.
+
+let a= 10
+let b=90
+console.log("4. Sum of a and b :",(a+b))
+
+//5. Multiplication:Declare a variable `x` with the value of 10 and `y` with the value of 15.
+//  Print the result of their multiplication.
+
+let x=10
+let y=15
+console.log("5. Multiplication of x and y is: "+(x*y))
+
+// 6.Perimeter of a Rectangle:Declare two variables `length` and `width`.
+//  Assign values to them and calculate the perimeter of the rectangle. Print the result.
+
+let length=8
+let width= 9
+let perimeterofRectangle= 2*(length+width)
+console.log("6. Perimeter of Rectancle= "+ perimeterofRectangle)
